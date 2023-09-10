f9d9634713e9578b41a6b4169cdf70d8495545f9
Pedro <pedro.enrique1599@icloud.com>
2023-09-10 00:26:17 -0300

    text field component added

--- organo/src/components/Banner/Banner.js
+++ organo/src/components/Banner/Banner.js
@@ -1,12 +1,10 @@
 import banner from '../../assets/images/banner.png';
 import './Banner.css';
 
-function Banner() {
+export const Banner = () => {
     return (
         <header className='banner'>
             <img src={banner} alt='Banner' />
         </header>
     );
 }
-
-export default Banner;
